bfe8724dcbac4942cdfa3c86f294ec0d18c0a958
Truong Tho <terynguyen2262001@gmail.com>
2023-11-02 02:37:10 +0700

    update: remove redudant line

--- app/src/main/java/com/example/phamngocquang/MainActivity.java
+++ app/src/main/java/com/example/phamngocquang/MainActivity.java
@@ -11,7 +11,6 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
